45cd08955a83877e10122212cf79f42e069a430e
Berresu <90646787+Berresu@users.noreply.github.com>
2022-04-21 14:40:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function arttir(){
+    let sayi=document.getElementById("sayi");
+    sayi.value=Number(sayi.value)+1;
+}
+
+function azalt(){
+    let sayi=document.getElementById("sayi");
+    sayi.value=Number(sayi.value)-1;
+}
+
+function sifirla(){
+    let sayi=document.getElementById("sayi");
+    sayi.value=0;
+}
